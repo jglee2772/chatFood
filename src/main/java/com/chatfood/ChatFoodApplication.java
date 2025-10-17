3463ed6946d2f5ce9d309f9119c2468118d07d3d
김동우 <alsgheptm@gmail.com>
2025-10-17 12:57:31 +0900

    1017 1257  woo

--- src/main/java/com/chatfood/ChatFoodApplication.java
+++ src/main/java/com/chatfood/ChatFoodApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChatFoodApplication {
+public class  ChatFoodApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ChatFoodApplication.class, args);
